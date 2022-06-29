716938288a2cc2095c5dd4c48534dc7ca9e3be2f
xujiajun <1878078284@qq.com>
2022-06-29 15:07:37 +0800

    项目初始化构建

---
+++ coupon/src/test/java/com/guigu/mall/coupon/CouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guigu.mall.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
